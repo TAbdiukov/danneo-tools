8807763cb3ff34df54c73cb1444cb13a0c090e65
Tim <tabdiukov@gmail.com>
2019-12-04 04:12:59 +1100

    have SOME working scripts

--- init.py
+++ init.py
@@ -1,118 +1,34 @@
 #!/usr/bin/python3
-import sys  
-import zlib
-import time
-import os
-import re
+import configparser
 
-#import requests-futures
-from baseconv import base62
-from etaprogress.progress import ProgressBar
-import requests
+
+	
+def APP_NAME():
+	return "danneo-tools"
+
+def APP_SHORT():
+	return "dan-tools"
+	
+	
+def finprint():
+	print(__file__ + " finished successfully")
 
 def main():
-	PROGRAM_NAME = "bing2"
-	SLEEP_TIME = 1	
+	WEBSITE = "DEFAULT"
+	#c = config[]
+	
+	config = configparser.ConfigParser()
+	config[WEBSITE] = {
+		'url': 'http://example.com', 
+		'user_cookie': '123userCRAZZZY',
+		'user_id_max': 25000
+	}
 
-	HOW_TO = """bing2 - smartened (brute)Bing
-	Use for targeted patterned attack"""
+	with open(APP_SHORT() + ".ini", 'w') as configfile:
+		config.write(configfile)	
+
+	print("init.py done successfully")
 	
-	if len(sys.argv) != 4: #-1
-		print("help here")
-	else:
-		# init		
-		URL = str(sys.argv[1])
-		length = max(len(sys.argv[2]), len(sys.argv[3]))
-		brute_min = int(base62.decode(sys.argv[2]))
-		brute_max = int(base62.decode(sys.argv[3]))
-		
-		logname = PROGRAM_NAME+"_"+getUnixTime()+".log"
-		log = open(logname, "a+")
-		
-		cookies = {
-			"zid": "z5214048",
-			"token": "???",
-			"session": "???"
-		}
-		
-		headers = {
-			"Upgrade-Insecure-Requests": "1"
-		}
-		
-		work = brute_max-brute_min
-		bar = ProgressBar(work, max_width=40)
-		
-		# print for user
-		print(PROGRAM_NAME+" initialised")
-		print("URL: "+URL)
-		print("Note: base62 is as follows: numbers->CAPITALS->lowercase")
-		print("stdout will be logged to "+logname)
-		print("the bruteforce will start in 3s")
-		# allow user to change brute_mind
-		time.sleep(3)
-		
-		#payload
-		i = 0
-		for i in range(work+1):
-			k=i+brute_min
-			
-			k_string = base62.encode(k) # convertion to the text
-			k_string = k_string.zfill(length) #decorating
-			
-			r = requests.get(URL+k_string, cookies=cookies, headers=headers)
-			txt = r.text
-			# https://docs.python.org/3/library/zlib.html SAYS
-			#'An Adler-32 checksum is almost as reliable as a CRC32 but can be computed much more quickly'
-			#'Changed in version 3.0: Always returns an unsigned value' => GOOD
-			txt_hash = toHexCustom(zlib.adler32(txt.encode('utf-8')))
-			
-			# write to payload listings
-			f_payload = open("pay_"+txt_hash+".txt", "a+")
-			f_payload.write(k_string+"\n")
-			f_payload.close()
-			
-			# if no transcription => first time resp encountered
-			if not(os.path.isfile("plain_"+txt_hash+".txt")):
-				# write to plaintext transcription
-				f_plain = open("plain_"+txt_hash+".txt", "w+", encoding="utf-8")
-				f_plain.write(txt)
-				f_plain.close()
-				# now log stuff
-				whatToLog = "[N]"+k_string+"; New hash found! Check: "+txt_hash+" ("+str(r.status_code)+")"
-				
-				log.write(whatToLog+"\n")
-				print(whatToLog)
-			
-			# if hash already encountered
-			else:
-				# boring log, what else to do
-				whatToLog = "[B]"+k_string+": "+txt_hash+" ("+str(r.status_code)+")"
-				
-				log.write(whatToLog+"\n")
-				print(whatToLog)
-				
-			bar.numerator = i
-			print(str(bar))
-			#sys.stdout.flush()
-			
-			#myCoolTitle = PROGRAM_NAME+" "+k_string
-			#os.system("title "+myCoolTitle) #https://stackoverflow.com/a/10229529
-			#time.sleep(SLEEP_TIME/1000)
-		
-		#payload (for-loop) over
-		whatToLog = "[F] Fin"
-				
-		log.write(whatToLog+"\n")
-		print(whatToLog)
-		
-		log.close()
-	
-	
-def getUnixTime():
-	return toHexCustom(int(time.time()))
-	
-def toHexCustom(dec): 
-	return str(hex(dec).split('x')[-1])	
-	
+# BEGIN
 if __name__ == '__main__':
 	main()
---
+++ test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print(__file__)
